8c183306d82537742abc5c3bb30c1d21f1fc8159
0xKurt <Kurt@Merbeth.io>
2023-08-27 10:48:44 +0200

    burnable

--- NFT/Challenge_NFT.sol
+++ NFT/Challenge_NFT.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Challenge_NFT is ERC721, Ownable {
+contract Challenge_NFT is ERC721, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
